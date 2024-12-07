e01cb659b7b6af5ad49df5795bc8c109df47b1f3
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-12-07 19:29:55 +0200

    Reapply "security considers"
    This reverts commit a59e147c6fe65bb842a5dbe6e44b9d1214d5753f.

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAh61dWtgbPdnq65nvVOp4eCc3y6FU0FHs",
-  authDomain: "bishell.firebaseapp.com",
-  projectId: "bishell",
-  storageBucket: "bishell.firebasestorage.app",
-  messagingSenderId: "307741969922",
-  appId: "1:307741969922:web:c54b2131957d9acf37611b",
-  measurementId: "G-EEF22G6JTR",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASURMENT_ID,
 };
 
 // Initialize Firebase
