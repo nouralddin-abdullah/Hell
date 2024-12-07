9980bb150be62446c309805d567518bb2e68017f
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-12-07 09:36:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/auth/login";
