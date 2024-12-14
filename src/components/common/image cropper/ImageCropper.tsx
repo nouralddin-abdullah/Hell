819fd002fa4cc79b09a1e9cc394e933813bb2ef9
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-12-14 16:46:01 +0200

    image cropper

--- src/components/common/image cropper/ImageCropper.tsx
+++ src/components/common/image cropper/ImageCropper.tsx
@@ -25,6 +25,54 @@ interface ImageCropperProps {
   imgClickRef?: React.RefObject<HTMLImageElement>;
 }
 
+const compressImage = (file: File): Promise<File> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+      const img = new Image();
+      img.src = event.target?.result as string;
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        // change max width and height to 1200 * 1200 34an keep el 1:1 aspect ratio
+        const MAX_WIDTH = 1200;
+        const MAX_HEIGHT = 1200;
+        let width = img.width;
+        let height = img.height;
+
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+
+        ctx?.drawImage(img, 0, 0, width, height);
+
+        canvas.toBlob((blob) => {
+          if (blob) {
+            const compressedFile = new File([blob], file.name, {
+              type: file.type,
+              lastModified: Date.now(),
+            });
+            resolve(compressedFile);
+          }
+        }, file.type);
+      };
+    };
+  });
+};
+
 const ImageCropper: React.FC<ImageCropperProps> = ({
   imgState,
   submitFunction,
@@ -122,7 +170,7 @@ const ImageCropper: React.FC<ImageCropperProps> = ({
     };
   };
 
-  const handleCropAndUpload = () => {
+  const handleCropAndUpload = async () => {
     if (imgRef.current && previewCanvasRef.current && crop) {
       setCanvasPreview(
         imgRef.current,
@@ -138,8 +186,9 @@ const ImageCropper: React.FC<ImageCropperProps> = ({
       const blob = dataURLtoBlob(dataUrl);
       const file = new File([blob], "croppedImage.png", { type: blob.type });
 
-      console.log(file);
-      submitFunction(imgAttr, file);
+      const compressedFile = await compressImage(file);
+
+      submitFunction(imgAttr, compressedFile);
       setImgSrc("");
     }
   };
@@ -195,7 +244,9 @@ const ImageCropper: React.FC<ImageCropperProps> = ({
             <button className="discard-btn" onClick={() => setImgSrc("")}>
               close
             </button>
-            <Button onClick={handleCropAndUpload}>Apply</Button>
+            <Button onClick={handleCropAndUpload} type="button">
+              Apply
+            </Button>
           </div>
 
           {crop && (
