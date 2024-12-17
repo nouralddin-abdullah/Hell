f053597db0d8ec8d3a3acd7c09b3f006bb8db246
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-12-17 18:15:17 +0200

    test insassllah work

--- src/hooks/posts/useGetPost.ts
+++ src/hooks/posts/useGetPost.ts
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { baseURL } from "../../constants/baseURL";
 import { Post } from "../../types/PostPreview";
-// import Cookies from "js-cookie";
-// import { tokenKey } from "../../constants/tokenKey";
+import Cookies from "js-cookie";
+import { tokenKey } from "../../constants/tokenKey";
 
 export const useGetPost = (
   username: string | undefined,
@@ -13,26 +13,21 @@ export const useGetPost = (
     queryFn: async () => {
       if (!postId || !username) return;
 
-      // const accessToken = Cookies.get(tokenKey);
+      const accessToken = Cookies.get(tokenKey);
+      const url = `${baseURL}/api/posts/${username}/${postId}`;
+      const headers: HeadersInit = {};
 
-      // if (!accessToken) {
-      //   throw new Error("No access token found");
-      // }
+      if (accessToken) {
+        headers.Authorization = `Bearer ${accessToken}`;
+      }
 
-      let url = `${baseURL}/api/posts/${username}/${postId}`;
-
-      const response = await fetch(url, {
-        // headers: {
-        //   Authorization: `Bearer ${accessToken}`,
-        // },
-      });
+      const response = await fetch(url, { headers });
 
       if (!response.ok) {
         throw new Error("Failed to fetch posts");
       }
 
       const data = await response.json();
-
       return data.data.post as Post;
     },
     enabled: !!postId,
